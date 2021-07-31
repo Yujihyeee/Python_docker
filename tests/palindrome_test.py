9e03bcf73f3a65ff137b63faf4745273aa36afdc
Yujihyeee <rladnddl@naver.com>
2021-07-31 13:09:59 +0900

    12

--- tests/palindrome_test.py
+++ tests/palindrome_test.py
@@ -4,11 +4,15 @@ from basic.palindrome import Palindrome
 
 
 class PalindromeTest(unittest.TestCase):
-    mock = Palindrome()
 
+    def test_str_to_list(self):
+        ls = [i for i in "A man, a plan, a canal: Panama" if i.isalnum()]
+        print(f'test_str_to_list : {ls}')
 
-    def test_palindrome(self):
-        self.mock.str_to_list()
+    def test_isPalindrome(self) -> bool:
+        ls = [i for i in "A man, a plan, a canal: Panama" if i.isalnum()]
+        dict = {"RESULT": True for i in ls if ls.pop(0) != ls.pop()}
+        print(f'test_isPalindrome : {dict["RESULT"]}')
 
 
 if __name__ == '__main__':
